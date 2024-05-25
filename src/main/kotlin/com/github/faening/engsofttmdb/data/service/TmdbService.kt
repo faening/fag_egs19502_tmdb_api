1fd3b5792ca3f2fe120f10ba3151da621deac08d
faening <cmorais58@gmail.com>
2024-05-25 20:02:13 -0300

    feat: add tmdb service

---
+++ src/main/kotlin/com/github/faening/engsofttmdb/data/service/TmdbService.kt
@@ -0,0 +1,30 @@
+package com.github.faening.engsofttmdb.data.service
+
+import com.github.faening.engsofttmdb.data.model.*
+import org.springframework.stereotype.Service
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+@Service
+interface TmdbService {
+
+    @GET("authentication")
+    fun authentication(): Call<AuthenticationData>
+
+    @GET("discover/movie")
+    fun getAllMovies(): Call<ResponsePageData<MovieData>>
+
+    @GET("discover/tv")
+    fun getAllTvShows(): Call<ResponsePageData<TvData>>
+
+    @GET("genre/movie/list")
+    fun getAllGenres(): Call<GenrePageData>
+
+    @GET("movie/{movie_id}/credits")
+    fun getMovieCredits(@Path("movie_id") movieId: Int): Call<CreditsData>
+
+    @GET("movie/{movie_id}/reviews")
+    fun getMovieReviews(@Path("movie_id") movieId: Int): Call<ResponsePageData<ReviewData>>
+
+}
